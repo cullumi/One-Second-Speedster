2f6103515adc6768253e3629bc851799dc6beff8
Ian Cullum <17892900+expat1999@users.noreply.github.com>
2019-08-03 14:28:41 -0700

    Player Character
    + Player Character
    + Player Camera
    + Movement/Aiming Code Borrowed from "Thief Clone"

--- Level 1/Level 1.gd
+++ Level 1/Level 1.gd
@@ -1,5 +1,7 @@
 extends Spatial
 
+onready var real_time_cam = $"Real-Time Camera"
+
 var trolly_speed = 40
 
 func _ready():
@@ -8,3 +10,5 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("un-pause"):
 		get_tree().call_group("pausable", "unpause")
+		
+		real_time_cam.current = true
---
+++ Level 1/Player.gd
@@ -0,0 +1,70 @@
+extends KinematicBody
+
+export (int) var move_speed = 15
+export (int) var look_speed = 3
+
+onready var camera = $Camera
+
+var is_paused = true
+
+func _ready():
+	add_to_group("pausable")
+	$MeshInstance.visible = false
+
+func _process(delta):
+	if is_paused:
+		aiming(delta)
+		movement()
+
+func movement():
+	var velocity = Vector3()
+	var direction = Vector3()
+	var forward_direction = rotation_degrees.y
+	
+	var is_moving = false
+	if Input.is_action_pressed("move_left"):
+		direction.x += sin(deg2rad(forward_direction - 90))
+		direction.z += cos(deg2rad(forward_direction - 90))
+		is_moving = true
+	if Input.is_action_pressed("move_right"):
+		direction.x += sin(deg2rad(forward_direction + 90))
+		direction.z += cos(deg2rad(forward_direction + 90))
+		is_moving = true
+	if Input.is_action_pressed("move_forward"):
+		direction.x += sin(deg2rad(forward_direction - 180))
+		direction.z += cos(deg2rad(forward_direction - 180))
+		is_moving = true
+	if Input.is_action_pressed("move_back"):
+		direction.x += sin(deg2rad(forward_direction))
+		direction.z += cos(deg2rad(forward_direction))
+		is_moving = true
+	velocity = direction.normalized() * move_speed
+	
+	if !is_on_floor():
+		velocity.y = -move_speed
+	else:
+		velocity += -get_slide_collision(0).normal * 100
+	
+	move_and_slide(velocity, Vector3.UP)
+
+func aiming(delta):
+	var velocity = Vector3()
+	var direction = Vector3()
+	
+	if Input.is_action_pressed("look_left"):
+		direction.y += 1;
+	if Input.is_action_pressed("look_right"):
+		direction.y -= 1;
+	if Input.is_action_pressed("look_up"):
+		direction.x += 1
+	if Input.is_action_pressed("look_down"):
+		direction.x -= 1;
+	velocity = direction.normalized() * look_speed
+	
+	var look_velocity = velocity * delta
+	camera.rotate_x(look_velocity.x)
+	rotate_y(look_velocity.y)
+	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -75, 75)
+
+func unpause():
+	is_paused = false
