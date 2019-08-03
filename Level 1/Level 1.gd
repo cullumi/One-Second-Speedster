1530068e10a036b4b7a81e1476255930f1d7777b
Ian Cullum <17892900+expat1999@users.noreply.github.com>
2019-08-02 23:37:10 -0700

    Level 1 -- Lighting / Detail / Improvments
    + Added Directional Light
    + Fixed jittery path following
    + Added "trolly" group for setting speed with "set_speed" methods
    + Quadrupled trolly speed
    + Implemented more detailed building mesh
    + Replaced placeholder building meshes
    + Added more buildings along left trolly path

--- Level 1/Level 1.gd
+++ Level 1/Level 1.gd
@@ -1,5 +1,10 @@
 extends Spatial
 
+var trolly_speed = 40
+
+func _ready():
+	get_tree().call_group("trolly", "set_speed", trolly_speed)
+
 func _process(delta):
 	if Input.is_action_just_pressed("un-pause"):
 		get_tree().call_group("pausable", "unpause")
--- Level 1/Trolly.gd
+++ Level 1/Trolly.gd
@@ -3,11 +3,13 @@ extends KinematicBody
 export (NodePath) var left_path_follow
 export (NodePath) var right_path_follow
 
+export (bool) var is_following_left_path = false
+
 var is_paused = true
 var speed = 10
-var is_following_left_path = true
 
 func _ready():
+	add_to_group("trolly")
 	add_to_group("pausable")
 
 func _process(delta):
@@ -15,14 +17,29 @@ func _process(delta):
 		
 		var velocity = Vector3()
 		var direction = Vector3()
+		var target_pos = Vector3()
+		var self_pos = global_transform.origin
 		
 		if is_following_left_path:
-			direction = global_transform.origin.direction_to(get_node(left_path_follow).global_transform.origin)
+			target_pos = get_node(left_path_follow).global_transform.origin
 		else:
-			direction = global_transform.origin.direction_to(get_node(right_path_follow).global_transform.origin)
+			target_pos = get_node(right_path_follow).global_transform.origin
 		
-		velocity = direction * speed
-		move_and_slide(velocity, Vector3.UP)
+		direction = self_pos.direction_to(target_pos)
+		
+		var tf = get_global_transform()
+		if Vector2(direction.x, direction.z).length_squared() > 0.001:
+			tf.basis = Basis(Vector3(0, 1, 0), atan2(-direction.x, -direction.z))
+#		tf.origin += direction * delta
+		set_global_transform(tf)
+		
+		if self_pos.distance_to(target_pos) >= self_pos.distance_to((self_pos + (direction * speed * delta))):
+			velocity = direction * speed
+			move_and_slide(velocity, Vector3.UP)
 
 func unpause():
 	is_paused = false
+
+func set_speed(new_speed):
+	self.speed = new_speed
+	print(self.speed)
